f777f554502e99bf08532e5b541926ea4b2c6f0a
Jose Sanchez <hailex@live.com>
2014-06-19 20:38:33 +0000

    Added facebook authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Clipbox::Application.routes.draw do
   
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy'#, as: => :destroy_user_session
+  end
 
   get 'about' => 'welcome#about'
 
