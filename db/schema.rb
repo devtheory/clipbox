10cbde0d503fa39ece2ecb9e06904faab2036d5b
Jose Sanchez <jparkers0012@gmail.com>
2014-06-19 20:38:33 +0000

    Added facebook authentication

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619163708) do
+ActiveRecord::Schema.define(version: 20140619194445) do
 
   create_table "users", force: true do |t|
     t.string   "name"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20140619163708) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
